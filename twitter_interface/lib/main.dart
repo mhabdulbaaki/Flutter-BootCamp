359fda56fd3add65194a799a646b8a961a9be488
mhabdulbaaki <mohammedhardi82@gmail.com>
2020-04-11 08:53:57 +0100

    appbar leading widget

--- twitter_interface/lib/main.dart
+++ twitter_interface/lib/main.dart
@@ -1,42 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blue,
-            title: Text('Home'),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Home'),
+          leading: CircleAvatar(
+            radius: 30.0,
+            backgroundColor: Colors.white,
+            backgroundImage: AssetImage('images/me.jpg'),
           ),
-          body: Center(
-            child: Image(
-              image: AssetImage('images/hydroponic.jpg'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/hydroponic.jpg'),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text('Home')),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: Text('Search'),
             ),
-          ),
-          backgroundColor: Colors.white,
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home), title: Text('Home')),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                title: Text('Search'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.notifications),
-                title: Text('Notification'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.email),
-                title: Text('Email'),
-              ),
-            ],
-            //backgroundColor: Colors.lightBlue,
-            fixedColor: Colors.blue,
-            unselectedItemColor: Colors.blueGrey,
-          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              title: Text('Notification'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.email),
+              title: Text('Email'),
+            ),
+          ],
+          //backgroundColor: Colors.lightBlue,
+          fixedColor: Colors.blue,
+          unselectedItemColor: Colors.blueGrey,
         ),
       ),
     );
+  }
+}
